ce43c22a90892c6bb7b3d502bdcf62cc80f5f295
gracegyu <gracegyu@1cbcf367-8df2-3845-b139-6d8f688f8976>
2012-10-11 14:03:55 +0000

    GTSudoku paid target 추가
    git-svn-id: http://10.211.55.4:81/svn/root/trunk/src/sudoku9@221 1cbcf367-8df2-3845-b139-6d8f688f8976

--- resource/CopyAllIcons.bat
+++ resource/CopyAllIcons.bat
@@ -1,4 +1,9 @@
-.\Sudoku9paid\CopyIcons.bat
-.\Sudoku9free\CopyIcons.bat
-.\Sudoku6free\CopyIcons.bat
+.\Sudoku9Paid\CopyIcons.bat
+.\Sudoku9Free\CopyIcons.bat
+.\Sudoku6Paid\CopyIcons.bat
+.\Sudoku6Free\CopyIcons.bat
+.\GTSudoku9Paid\CopyIcons.bat
+.\GTSudoku9Free\CopyIcons.bat
+.\GTSudoku6Paid\CopyIcons.bat
+.\GTSudoku6Free\CopyIcons.bat
 
---
+++ resource/GTSudoku6Paid/CopyIcons.bat
@@ -0,0 +1,14 @@
+"C:\Program Files\ImageMagick-6.7.9-Q16\convert.exe" GTSudoku6PaidIcon-1024.png -resize 29x29 GTSudoku6PaidIcon-Small.png 
+"C:\Program Files\ImageMagick-6.7.9-Q16\convert.exe" GTSudoku6PaidIcon-1024.png -resize 50x50 GTSudoku6PaidIcon-Small-50.png 
+"C:\Program Files\ImageMagick-6.7.9-Q16\convert.exe" GTSudoku6PaidIcon-1024.png -resize 57x57 GTSudoku6PaidIcon.png 
+"C:\Program Files\ImageMagick-6.7.9-Q16\convert.exe" GTSudoku6PaidIcon-1024.png -resize 58x58 GTSudoku6PaidIcon-Small@2x.png 
+"C:\Program Files\ImageMagick-6.7.9-Q16\convert.exe" GTSudoku6PaidIcon-1024.png -resize 72x72 GTSudoku6PaidIcon~ipad.png 
+"C:\Program Files\ImageMagick-6.7.9-Q16\convert.exe" GTSudoku6PaidIcon-1024.png -resize 72x72 GTSudoku6PaidIcon-72.png 
+"C:\Program Files\ImageMagick-6.7.9-Q16\convert.exe" GTSudoku6PaidIcon-1024.png -resize 114x114 GTSudoku6PaidIcon@2x.png 
+"C:\Program Files\ImageMagick-6.7.9-Q16\convert.exe" GTSudoku6PaidIcon-1024.png -resize 144x144 GTSudoku6PaidIcon@2x~ipad.png 
+"C:\Program Files\ImageMagick-6.7.9-Q16\convert.exe" GTSudoku6PaidIcon-1024.png -resize 512x512 GTSudoku6PaidIcon-512.png 
+
+
+"C:\Program Files\ImageMagick-6.7.9-Q16\convert.exe" GTSudoku6PaidDefault@2x.png -resize 320x480 GTSudoku6PaidDefault.png
+"C:\Program Files\ImageMagick-6.7.9-Q16\convert.exe" GTSudoku6PaidDefault-Landscape@2x~ipad.png -resize 1024x748 GTSudoku6PaidDefault-Landscape~ipad.png
+"C:\Program Files\ImageMagick-6.7.9-Q16\convert.exe" GTSudoku6PaidDefault-Portrait@2x~ipad.png -resize 768x1004 GTSudoku6PaidDefault-Portrait~ipad.png
