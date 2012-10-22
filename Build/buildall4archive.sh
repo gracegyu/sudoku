71e6983dffc593da22aa40867eb0191361859438
Raymond Jeon <gracegyu@gmail.com>
2012-10-22 22:32:42 +0900

    빌드 개

--- Build/buildall4archive.sh
+++ Build/buildall4archive.sh
@@ -2,6 +2,7 @@
 
 xcodebuild  -project ../sudokuall.xcodeproj  -scheme "sudoku6free" clean archive
 xcodebuild  -project ../sudokuall.xcodeproj  -scheme "sudoku6paid" clean archive
+xcodebuild  -project ../sudokuall.xcodeproj  -scheme "sudoku7free" clean archive
 xcodebuild  -project ../sudokuall.xcodeproj  -scheme "sudoku9free" clean archive
 xcodebuild  -project ../sudokuall.xcodeproj  -scheme "sudoku9paid" clean archive
 xcodebuild  -project ../sudokuall.xcodeproj  -scheme "gtsudoku6free" clean archive
