cef2bd73af27839fbaf75ce24ec20939ff0045e3
Raymond Jeon <gracegyu@gmail.com>
2012-10-27 21:08:10 +0900

    KillerSudoku6Free 등

---
+++ resource/KillerSudoku6Free/CopyIcons.bat
@@ -0,0 +1,10 @@
+convert.exe KillerSudoku6FreeIcon-1024.png -resize 29x29 KillerSudoku6FreeIcon-Small.png 
+convert.exe KillerSudoku6FreeIcon-1024.png -resize 50x50 KillerSudoku6FreeIcon-Small-50.png 
+convert.exe KillerSudoku6FreeIcon-1024.png -resize 57x57 KillerSudoku6FreeIcon.png 
+convert.exe KillerSudoku6FreeIcon-1024.png -resize 58x58 KillerSudoku6FreeIcon-Small@2x.png 
+convert.exe KillerSudoku6FreeIcon-1024.png -resize 72x72 KillerSudoku6FreeIcon~ipad.png 
+convert.exe KillerSudoku6FreeIcon-1024.png -resize 72x72 KillerSudoku6FreeIcon-72.png 
+convert.exe KillerSudoku6FreeIcon-1024.png -resize 114x114 KillerSudoku6FreeIcon@2x.png 
+convert.exe KillerSudoku6FreeIcon-1024.png -resize 144x144 KillerSudoku6FreeIcon@2x~ipad.png 
+convert.exe KillerSudoku6FreeIcon-1024.png -resize 512x512 KillerSudoku6FreeIcon-512.png 
+ 
