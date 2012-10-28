f96cdad480779fbe9eaab817c98aac88ba5bfdaa
Raymond Jeon <gracegyu@gmail.com>
2012-10-28 10:00:54 +0900

    KillerSudoku6Piad icon 등록

---
+++ resource/KillerSudoku6Paid/CopyIcons.bat
@@ -0,0 +1,10 @@
+convert.exe KillerSudoku6PaidIcon-1024.png -resize 29x29 KillerSudoku6PaidIcon-Small.png 
+convert.exe KillerSudoku6PaidIcon-1024.png -resize 50x50 KillerSudoku6PaidIcon-Small-50.png 
+convert.exe KillerSudoku6PaidIcon-1024.png -resize 57x57 KillerSudoku6PaidIcon.png 
+convert.exe KillerSudoku6PaidIcon-1024.png -resize 58x58 KillerSudoku6PaidIcon-Small@2x.png 
+convert.exe KillerSudoku6PaidIcon-1024.png -resize 72x72 KillerSudoku6PaidIcon~ipad.png 
+convert.exe KillerSudoku6PaidIcon-1024.png -resize 72x72 KillerSudoku6PaidIcon-72.png 
+convert.exe KillerSudoku6PaidIcon-1024.png -resize 114x114 KillerSudoku6PaidIcon@2x.png 
+convert.exe KillerSudoku6PaidIcon-1024.png -resize 144x144 KillerSudoku6PaidIcon@2x~ipad.png 
+convert.exe KillerSudoku6PaidIcon-1024.png -resize 512x512 KillerSudoku6PaidIcon-512.png 
+ 
