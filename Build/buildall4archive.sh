bad3a8cdd63e3ffb607f13712ab04c2bec0b45bc
Raymond Jeon <gracegyu@gmail.com>
2012-11-01 13:08:19 +0900

    Build Script 수

--- Build/buildall4archive.sh
+++ Build/buildall4archive.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-
+xcodebuild  -project ../sudokuall.xcodeproj  -scheme "sudoku6free" clean archive
+xcodebuild  -project ../sudokuall.xcodeproj  -scheme "sudoku6paid" clean archive
+xcodebuild  -project ../sudokuall.xcodeproj  -scheme "sudoku9free" clean archive
+xcodebuild  -project ../sudokuall.xcodeproj  -scheme "sudoku9paid" clean archive
+xcodebuild  -project ../sudokuall.xcodeproj  -scheme "gtsudoku6free" clean archive
+xcodebuild  -project ../sudokuall.xcodeproj  -scheme "gtsudoku6paid" clean archive
+xcodebuild  -project ../sudokuall.xcodeproj  -scheme "gtsudoku9free" clean archive
+xcodebuild  -project ../sudokuall.xcodeproj  -scheme "gtsudoku9paid" clean archive
+xcodebuild  -project ../sudokuall.xcodeproj  -scheme "killersudoku6free" clean archive
+xcodebuild  -project ../sudokuall.xcodeproj  -scheme "killersudoku6paid" clean archive
+xcodebuild  -project ../sudokuall.xcodeproj  -scheme "killersudoku9free" clean archive
+xcodebuild  -project ../sudokuall.xcodeproj  -scheme "killersudoku9paid" clean archive
 xcodebuild  -project ../sudokuall.xcodeproj  -scheme "calcudoku6free" clean archive
 xcodebuild  -project ../sudokuall.xcodeproj  -scheme "calcudoku6paid" clean archive
 xcodebuild  -project ../sudokuall.xcodeproj  -scheme "calcudoku9free" clean archive
