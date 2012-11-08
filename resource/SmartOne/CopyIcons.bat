1210c5cf83e447545bc32df4092a1845e1c052ba
Raymond Jeon <gracegyu@gmail.com>
2012-11-08 21:55:26 +0900

    SmartOne logo 추가

---
+++ resource/SmartOne/CopyIcons.bat
@@ -0,0 +1,7 @@
+
+ 
+convert.exe logo.png -resize 16x16 logo-16.png 
+convert.exe logo.png -resize 64x64 logo-64.png 
+convert.exe logo.png -resize 75x75 logo-75.png 
+convert.exe logo.png -resize 96x96 logo-96.png 
+convert.exe logo.png -resize 128x128 logo-128.png
