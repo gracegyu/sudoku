2fdf13fb3403a0915d9561a306b3589cb456a745
Raymond Jeon <gracegyu@gmail.com>
2012-11-22 09:54:06 +0900

    SDK-362 메모가 가운데 쏠리는 것이 불편하다고 한다.

--- Classes/MainView.m
+++ Classes/MainView.m
@@ -764,7 +764,8 @@ static NSUInteger RainbowColorTemplate[7] = {
 	int x, y;
 //    int countW = (len <= 1 ? 1 : (len <= 4 ? 2 : 3));
     int countW = (len <= 4 ? 2 : 3);
-    int countH = (len <= 2 ? 1 : (len <= 6 ? 2 : 3));
+//    int countH = (len <= 2 ? 1 : (len <= 6 ? 2 : 3));
+    int countH = (len <= 6 ? 2 : 3);
     BOOL bConflict;
 	CGRect rectNum;    
     CGFloat margin;
