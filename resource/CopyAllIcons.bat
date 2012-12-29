ac003fcfe2f7d72ef31107df73ee909a0f4f6d92
Raymond Jeon <gracegy@gmail.com>
2012-12-29 15:28:36 +0900

    icon 수정

--- resource/CopyAllIcons.bat
+++ resource/CopyAllIcons.bat
@@ -1,9 +1,14 @@
-.\Sudoku9Paid\CopyIcons.bat
-.\Sudoku9Free\CopyIcons.bat
-.\Sudoku6Paid\CopyIcons.bat
-.\Sudoku6Free\CopyIcons.bat
-.\GTSudoku9Paid\CopyIcons.bat
-.\GTSudoku9Free\CopyIcons.bat
-.\GTSudoku6Paid\CopyIcons.bat
-.\GTSudoku6Free\CopyIcons.bat
+@echo on
 
+cd Sudoku9Paid
+CopyIcons.bat
+cd ../
+cd Sudoku9Free
+CopyIcons.bat
+cd ../
+cd Sudoku6Paid
+CopyIcons.bat
+cd ../
+cd Sudoku6Free
+CopyIcons.bat
+cd ../
