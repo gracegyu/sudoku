72dd57764aea334e63fa42f70f10166a4d912092
Raymond Jeon(Mac) <gracegyu@gmail.com>
2014-09-21 18:16:06 +0900

    Icon Set 정리

--- resource/Sudoku6Paid/CopyIcons.bat
+++ resource/Sudoku6Paid/CopyIcons.bat
@@ -1,15 +1,24 @@
-convert.exe Sudoku6PaidIcon-1024.png -resize 29x29 Sudoku6PaidIcon-Small.png 
-convert.exe Sudoku6PaidIcon-1024.png -resize 50x50 Sudoku6PaidIcon-Small-50.png 
-convert.exe Sudoku6PaidIcon-1024.png -resize 57x57 Sudoku6PaidIcon.png 
-convert.exe Sudoku6PaidIcon-1024.png -resize 58x58 Sudoku6PaidIcon-Small@2x.png 
-convert.exe Sudoku6PaidIcon-1024.png -resize 72x72 Sudoku6PaidIcon~ipad.png 
-convert.exe Sudoku6PaidIcon-1024.png -resize 72x72 Sudoku6PaidIcon-72.png 
-convert.exe Sudoku6PaidIcon-1024.png -resize 114x114 Sudoku6PaidIcon@2x.png 
-convert.exe Sudoku6PaidIcon-1024.png -resize 144x144 Sudoku6PaidIcon@2x~ipad.png 
-convert.exe Sudoku6PaidIcon-1024.png -resize 512x512 Sudoku6PaidIcon-512.png 
-convert.exe Sudoku6PaidIcon-1024.png -resize 120x120 Sudoku6PaidIcon-60@2x.png 
-convert.exe Sudoku6PaidIcon-1024.png -resize 76x76 Sudoku6PaidIcon-76.png 
-convert.exe Sudoku6PaidIcon-1024.png -resize 152x152 Sudoku6PaidIcon-76@2x.png 
+convert.exe Sudoku6PaidIcon-1024.png -resize 29x29 Sudoku6PaidIcon-29.png
+convert.exe Sudoku6PaidIcon-1024.png -resize 58x58 Sudoku6PaidIcon-29@2x.png
+convert.exe Sudoku6PaidIcon-1024.png -resize 87x87 Sudoku6PaidIcon-29@3x.png
+
 convert.exe Sudoku6PaidIcon-1024.png -resize 40x40 Sudoku6PaidIcon-40.png 
 convert.exe Sudoku6PaidIcon-1024.png -resize 80x80 Sudoku6PaidIcon-40@2x.png 
-convert.exe Sudoku6PaidIcon-1024.png -resize 100x100 Sudoku6PaidIcon-100.png 
+convert.exe Sudoku6PaidIcon-1024.png -resize 120x120 Sudoku6PaidIcon-40@3x.png 
+
+convert.exe Sudoku6PaidIcon-1024.png -resize 60x60 Sudoku6PaidIcon-60.png 
+convert.exe Sudoku6PaidIcon-1024.png -resize 120x120 Sudoku6PaidIcon-60@2x.png 
+convert.exe Sudoku6PaidIcon-1024.png -resize 180x180 Sudoku6PaidIcon-60@3x.png 
+
+convert.exe Sudoku6PaidIcon-1024.png -resize 57x57 Sudoku6PaidIcon-57.png 
+convert.exe Sudoku6PaidIcon-1024.png -resize 114x114 Sudoku6PaidIcon-57@2x.png 
+ 
+convert.exe Sudoku6PaidIcon-1024.png -resize 50x50 Sudoku6PaidIcon-50.png 
+convert.exe Sudoku6PaidIcon-1024.png -resize 100x100 Sudoku6PaidIcon-50@2x.png 
+
+convert.exe Sudoku6PaidIcon-1024.png -resize 72x72 Sudoku6PaidIcon-72.png 
+convert.exe Sudoku6PaidIcon-1024.png -resize 144x144 Sudoku6PaidIcon-72@2x.png 
+
+convert.exe Sudoku6PaidIcon-1024.png -resize 512x512 Sudoku6PaidIcon-512.png 
+convert.exe Sudoku6PaidIcon-1024.png -resize 76x76 Sudoku6PaidIcon-76.png 
+convert.exe Sudoku6PaidIcon-1024.png -resize 152x152 Sudoku6PaidIcon-76@2x.png 
--- resource/Sudoku9Paid/CopyIcons.bat
+++ resource/Sudoku9Paid/CopyIcons.bat
@@ -1,15 +1,24 @@
-convert.exe Sudoku9PaidIcon-1024.png -resize 29x29 Sudoku9PaidIcon-Small.png 
-convert.exe Sudoku9PaidIcon-1024.png -resize 50x50 Sudoku9PaidIcon-Small-50.png 
-convert.exe Sudoku9PaidIcon-1024.png -resize 57x57 Sudoku9PaidIcon.png 
-convert.exe Sudoku9PaidIcon-1024.png -resize 58x58 Sudoku9PaidIcon-Small@2x.png 
-convert.exe Sudoku9PaidIcon-1024.png -resize 72x72 Sudoku9PaidIcon~ipad.png 
-convert.exe Sudoku9PaidIcon-1024.png -resize 72x72 Sudoku9PaidIcon-72.png 
-convert.exe Sudoku9PaidIcon-1024.png -resize 114x114 Sudoku9PaidIcon@2x.png 
-convert.exe Sudoku9PaidIcon-1024.png -resize 144x144 Sudoku9PaidIcon@2x~ipad.png 
-convert.exe Sudoku9PaidIcon-1024.png -resize 512x512 Sudoku9PaidIcon-512.png 
-convert.exe Sudoku9PaidIcon-1024.png -resize 120x120 Sudoku9PaidIcon-60@2x.png 
-convert.exe Sudoku9PaidIcon-1024.png -resize 76x76 Sudoku9PaidIcon-76.png 
-convert.exe Sudoku9PaidIcon-1024.png -resize 152x152 Sudoku9PaidIcon-76@2x.png 
+convert.exe Sudoku9PaidIcon-1024.png -resize 29x29 Sudoku9PaidIcon-29.png
+convert.exe Sudoku9PaidIcon-1024.png -resize 58x58 Sudoku9PaidIcon-29@2x.png
+convert.exe Sudoku9PaidIcon-1024.png -resize 87x87 Sudoku9PaidIcon-29@3x.png
+
 convert.exe Sudoku9PaidIcon-1024.png -resize 40x40 Sudoku9PaidIcon-40.png 
 convert.exe Sudoku9PaidIcon-1024.png -resize 80x80 Sudoku9PaidIcon-40@2x.png 
-convert.exe Sudoku9PaidIcon-1024.png -resize 100x100 Sudoku9PaidIcon-100.png 
+convert.exe Sudoku9PaidIcon-1024.png -resize 120x120 Sudoku9PaidIcon-40@3x.png 
+
+convert.exe Sudoku9PaidIcon-1024.png -resize 60x60 Sudoku9PaidIcon-60.png 
+convert.exe Sudoku9PaidIcon-1024.png -resize 120x120 Sudoku9PaidIcon-60@2x.png 
+convert.exe Sudoku9PaidIcon-1024.png -resize 180x180 Sudoku9PaidIcon-60@3x.png 
+
+convert.exe Sudoku9PaidIcon-1024.png -resize 57x57 Sudoku9PaidIcon-57.png 
+convert.exe Sudoku9PaidIcon-1024.png -resize 114x114 Sudoku9PaidIcon-57@2x.png 
+ 
+convert.exe Sudoku9PaidIcon-1024.png -resize 50x50 Sudoku9PaidIcon-50.png 
+convert.exe Sudoku9PaidIcon-1024.png -resize 100x100 Sudoku9PaidIcon-50@2x.png 
+
+convert.exe Sudoku9PaidIcon-1024.png -resize 72x72 Sudoku9PaidIcon-72.png 
+convert.exe Sudoku9PaidIcon-1024.png -resize 144x144 Sudoku9PaidIcon-72@2x.png 
+
+convert.exe Sudoku9PaidIcon-1024.png -resize 512x512 Sudoku9PaidIcon-512.png 
+convert.exe Sudoku9PaidIcon-1024.png -resize 76x76 Sudoku9PaidIcon-76.png 
+convert.exe Sudoku9PaidIcon-1024.png -resize 152x152 Sudoku9PaidIcon-76@2x.png 
