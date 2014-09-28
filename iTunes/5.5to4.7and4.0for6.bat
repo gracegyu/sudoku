46978238348f221f9ed16a3f530fb06529a6f7a1
Raymond Jeon(Mac) <gracegyu@gmail.com>
2014-09-28 18:00:24 +0900

    iTunes 이미지 등록

---
+++ iTunes/5.5to4.7and4.0for6.bat
@@ -0,0 +1,59 @@
+convert.exe sudoku6/en/5.5inch-1.jpg -resize 750x1334! sudoku6/en/4.7inch-1.jpg
+convert.exe sudoku6/en/5.5inch-2.jpg -resize 750x1334! sudoku6/en/4.7inch-2.jpg
+convert.exe sudoku6/en/5.5inch-3.jpg -resize 750x1334! sudoku6/en/4.7inch-3.jpg
+convert.exe sudoku6/en/5.5inch-4.jpg -resize 750x1334! sudoku6/en/4.7inch-4.jpg
+convert.exe sudoku6/en/5.5inch-5.jpg -resize 750x1334! sudoku6/en/4.7inch-5.jpg
+
+convert.exe sudoku6/en/5.5inch-1.jpg -resize 640x1136! sudoku6/en/4.0inch-1.jpg
+convert.exe sudoku6/en/5.5inch-2.jpg -resize 640x1136! sudoku6/en/4.0inch-2.jpg
+convert.exe sudoku6/en/5.5inch-3.jpg -resize 640x1136! sudoku6/en/4.0inch-3.jpg
+convert.exe sudoku6/en/5.5inch-4.jpg -resize 640x1136! sudoku6/en/4.0inch-4.jpg
+convert.exe sudoku6/en/5.5inch-5.jpg -resize 640x1136! sudoku6/en/4.0inch-5.jpg
+
+convert.exe sudoku6/ja/5.5inch-1.jpg -resize 750x1334! sudoku6/ja/4.7inch-1.jpg
+convert.exe sudoku6/ja/5.5inch-2.jpg -resize 750x1334! sudoku6/ja/4.7inch-2.jpg
+convert.exe sudoku6/ja/5.5inch-3.jpg -resize 750x1334! sudoku6/ja/4.7inch-3.jpg
+convert.exe sudoku6/ja/5.5inch-4.jpg -resize 750x1334! sudoku6/ja/4.7inch-4.jpg
+convert.exe sudoku6/ja/5.5inch-5.jpg -resize 750x1334! sudoku6/ja/4.7inch-5.jpg
+
+convert.exe sudoku6/ja/5.5inch-1.jpg -resize 640x1136! sudoku6/ja/4.0inch-1.jpg
+convert.exe sudoku6/ja/5.5inch-2.jpg -resize 640x1136! sudoku6/ja/4.0inch-2.jpg
+convert.exe sudoku6/ja/5.5inch-3.jpg -resize 640x1136! sudoku6/ja/4.0inch-3.jpg
+convert.exe sudoku6/ja/5.5inch-4.jpg -resize 640x1136! sudoku6/ja/4.0inch-4.jpg
+convert.exe sudoku6/ja/5.5inch-5.jpg -resize 640x1136! sudoku6/ja/4.0inch-5.jpg
+
+convert.exe sudoku6/ko/5.5inch-1.jpg -resize 750x1334! sudoku6/ko/4.7inch-1.jpg
+convert.exe sudoku6/ko/5.5inch-2.jpg -resize 750x1334! sudoku6/ko/4.7inch-2.jpg
+convert.exe sudoku6/ko/5.5inch-3.jpg -resize 750x1334! sudoku6/ko/4.7inch-3.jpg
+convert.exe sudoku6/ko/5.5inch-4.jpg -resize 750x1334! sudoku6/ko/4.7inch-4.jpg
+convert.exe sudoku6/ko/5.5inch-5.jpg -resize 750x1334! sudoku6/ko/4.7inch-5.jpg
+
+convert.exe sudoku6/ko/5.5inch-1.jpg -resize 640x1136! sudoku6/ko/4.0inch-1.jpg
+convert.exe sudoku6/ko/5.5inch-2.jpg -resize 640x1136! sudoku6/ko/4.0inch-2.jpg
+convert.exe sudoku6/ko/5.5inch-3.jpg -resize 640x1136! sudoku6/ko/4.0inch-3.jpg
+convert.exe sudoku6/ko/5.5inch-4.jpg -resize 640x1136! sudoku6/ko/4.0inch-4.jpg
+convert.exe sudoku6/ko/5.5inch-5.jpg -resize 640x1136! sudoku6/ko/4.0inch-5.jpg
+
+convert.exe sudoku6/zh_CN/5.5inch-1.jpg -resize 750x1334! sudoku6/zh_CN/4.7inch-1.jpg
+convert.exe sudoku6/zh_CN/5.5inch-2.jpg -resize 750x1334! sudoku6/zh_CN/4.7inch-2.jpg
+convert.exe sudoku6/zh_CN/5.5inch-3.jpg -resize 750x1334! sudoku6/zh_CN/4.7inch-3.jpg
+convert.exe sudoku6/zh_CN/5.5inch-4.jpg -resize 750x1334! sudoku6/zh_CN/4.7inch-4.jpg
+convert.exe sudoku6/zh_CN/5.5inch-5.jpg -resize 750x1334! sudoku6/zh_CN/4.7inch-5.jpg
+
+convert.exe sudoku6/zh_CN/5.5inch-1.jpg -resize 640x1136! sudoku6/zh_CN/4.0inch-1.jpg
+convert.exe sudoku6/zh_CN/5.5inch-2.jpg -resize 640x1136! sudoku6/zh_CN/4.0inch-2.jpg
+convert.exe sudoku6/zh_CN/5.5inch-3.jpg -resize 640x1136! sudoku6/zh_CN/4.0inch-3.jpg
+convert.exe sudoku6/zh_CN/5.5inch-4.jpg -resize 640x1136! sudoku6/zh_CN/4.0inch-4.jpg
+convert.exe sudoku6/zh_CN/5.5inch-5.jpg -resize 640x1136! sudoku6/zh_CN/4.0inch-5.jpg
+
+convert.exe sudoku6/zh_TW/5.5inch-1.jpg -resize 750x1334! sudoku6/zh_TW/4.7inch-1.jpg
+convert.exe sudoku6/zh_TW/5.5inch-2.jpg -resize 750x1334! sudoku6/zh_TW/4.7inch-2.jpg
+convert.exe sudoku6/zh_TW/5.5inch-3.jpg -resize 750x1334! sudoku6/zh_TW/4.7inch-3.jpg
+convert.exe sudoku6/zh_TW/5.5inch-4.jpg -resize 750x1334! sudoku6/zh_TW/4.7inch-4.jpg
+convert.exe sudoku6/zh_TW/5.5inch-5.jpg -resize 750x1334! sudoku6/zh_TW/4.7inch-5.jpg
+
+convert.exe sudoku6/zh_TW/5.5inch-1.jpg -resize 640x1136! sudoku6/zh_TW/4.0inch-1.jpg
+convert.exe sudoku6/zh_TW/5.5inch-2.jpg -resize 640x1136! sudoku6/zh_TW/4.0inch-2.jpg
+convert.exe sudoku6/zh_TW/5.5inch-3.jpg -resize 640x1136! sudoku6/zh_TW/4.0inch-3.jpg
+convert.exe sudoku6/zh_TW/5.5inch-4.jpg -resize 640x1136! sudoku6/zh_TW/4.0inch-4.jpg
+convert.exe sudoku6/zh_TW/5.5inch-5.jpg -resize 640x1136! sudoku6/zh_TW/4.0inch-5.jpg
