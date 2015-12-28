a99fb222ba5fbfe5d9bd17e38ea8d725b9c62e8a
Raymond Jeon(Mac) <gracegyu@gmail.com>
2015-12-28 21:36:38 +0900

    83.5 icon add

--- resource/Sudoku6Free/CopyIcons.bat
+++ resource/Sudoku6Free/CopyIcons.bat
@@ -1,24 +1,26 @@
-convert.exe Sudoku6FreeIcon-1024.png -resize 29x29 Sudoku6FreeIcon-29.png
-convert.exe Sudoku6FreeIcon-1024.png -resize 58x58 Sudoku6FreeIcon-29@2x.png
-convert.exe Sudoku6FreeIcon-1024.png -resize 87x87 Sudoku6FreeIcon-29@3x.png
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 29x29 Sudoku6FreeIcon-29.png
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 58x58 Sudoku6FreeIcon-29@2x.png
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 87x87 Sudoku6FreeIcon-29@3x.png
 
-convert.exe Sudoku6FreeIcon-1024.png -resize 40x40 Sudoku6FreeIcon-40.png 
-convert.exe Sudoku6FreeIcon-1024.png -resize 80x80 Sudoku6FreeIcon-40@2x.png 
-convert.exe Sudoku6FreeIcon-1024.png -resize 120x120 Sudoku6FreeIcon-40@3x.png 
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 40x40 Sudoku6FreeIcon-40.png 
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 80x80 Sudoku6FreeIcon-40@2x.png 
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 120x120 Sudoku6FreeIcon-40@3x.png 
 
-convert.exe Sudoku6FreeIcon-1024.png -resize 60x60 Sudoku6FreeIcon-60.png 
-convert.exe Sudoku6FreeIcon-1024.png -resize 120x120 Sudoku6FreeIcon-60@2x.png 
-convert.exe Sudoku6FreeIcon-1024.png -resize 180x180 Sudoku6FreeIcon-60@3x.png 
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 60x60 Sudoku6FreeIcon-60.png 
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 120x120 Sudoku6FreeIcon-60@2x.png 
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 180x180 Sudoku6FreeIcon-60@3x.png 
 
-convert.exe Sudoku6FreeIcon-1024.png -resize 57x57 Sudoku6FreeIcon-57.png 
-convert.exe Sudoku6FreeIcon-1024.png -resize 114x114 Sudoku6FreeIcon-57@2x.png 
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 57x57 Sudoku6FreeIcon-57.png 
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 114x114 Sudoku6FreeIcon-57@2x.png 
  
-convert.exe Sudoku6FreeIcon-1024.png -resize 50x50 Sudoku6FreeIcon-50.png 
-convert.exe Sudoku6FreeIcon-1024.png -resize 100x100 Sudoku6FreeIcon-50@2x.png 
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 50x50 Sudoku6FreeIcon-50.png 
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 100x100 Sudoku6FreeIcon-50@2x.png 
 
-convert.exe Sudoku6FreeIcon-1024.png -resize 72x72 Sudoku6FreeIcon-72.png 
-convert.exe Sudoku6FreeIcon-1024.png -resize 144x144 Sudoku6FreeIcon-72@2x.png 
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 72x72 Sudoku6FreeIcon-72.png 
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 144x144 Sudoku6FreeIcon-72@2x.png 
 
-convert.exe Sudoku6FreeIcon-1024.png -resize 512x512 Sudoku6FreeIcon-512.png 
-convert.exe Sudoku6FreeIcon-1024.png -resize 76x76 Sudoku6FreeIcon-76.png 
-convert.exe Sudoku6FreeIcon-1024.png -resize 152x152 Sudoku6FreeIcon-76@2x.png 
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 512x512 Sudoku6FreeIcon-512.png 
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 76x76 Sudoku6FreeIcon-76.png 
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 152x152 Sudoku6FreeIcon-76@2x.png 
+
+..\convert.exe Sudoku6FreeIcon-1024.png -resize 167x167 Sudoku6FreeIcon-83.5@2x.png 
--- resource/Sudoku9Free/CopyIcons.bat
+++ resource/Sudoku9Free/CopyIcons.bat
@@ -1,24 +1,26 @@
-convert.exe Sudoku9FreeIcon-1024.png -resize 29x29 Sudoku9FreeIcon-29.png
-convert.exe Sudoku9FreeIcon-1024.png -resize 58x58 Sudoku9FreeIcon-29@2x.png
-convert.exe Sudoku9FreeIcon-1024.png -resize 87x87 Sudoku9FreeIcon-29@3x.png
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 29x29 Sudoku9FreeIcon-29.png
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 58x58 Sudoku9FreeIcon-29@2x.png
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 87x87 Sudoku9FreeIcon-29@3x.png
 
-convert.exe Sudoku9FreeIcon-1024.png -resize 40x40 Sudoku9FreeIcon-40.png 
-convert.exe Sudoku9FreeIcon-1024.png -resize 80x80 Sudoku9FreeIcon-40@2x.png 
-convert.exe Sudoku9FreeIcon-1024.png -resize 120x120 Sudoku9FreeIcon-40@3x.png 
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 40x40 Sudoku9FreeIcon-40.png 
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 80x80 Sudoku9FreeIcon-40@2x.png 
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 120x120 Sudoku9FreeIcon-40@3x.png 
 
-convert.exe Sudoku9FreeIcon-1024.png -resize 60x60 Sudoku9FreeIcon-60.png 
-convert.exe Sudoku9FreeIcon-1024.png -resize 120x120 Sudoku9FreeIcon-60@2x.png 
-convert.exe Sudoku9FreeIcon-1024.png -resize 180x180 Sudoku9FreeIcon-60@3x.png 
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 60x60 Sudoku9FreeIcon-60.png 
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 120x120 Sudoku9FreeIcon-60@2x.png 
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 180x180 Sudoku9FreeIcon-60@3x.png 
 
-convert.exe Sudoku9FreeIcon-1024.png -resize 57x57 Sudoku9FreeIcon-57.png 
-convert.exe Sudoku9FreeIcon-1024.png -resize 114x114 Sudoku9FreeIcon-57@2x.png 
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 57x57 Sudoku9FreeIcon-57.png 
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 114x114 Sudoku9FreeIcon-57@2x.png 
  
-convert.exe Sudoku9FreeIcon-1024.png -resize 50x50 Sudoku9FreeIcon-50.png 
-convert.exe Sudoku9FreeIcon-1024.png -resize 100x100 Sudoku9FreeIcon-50@2x.png 
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 50x50 Sudoku9FreeIcon-50.png 
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 100x100 Sudoku9FreeIcon-50@2x.png 
 
-convert.exe Sudoku9FreeIcon-1024.png -resize 72x72 Sudoku9FreeIcon-72.png 
-convert.exe Sudoku9FreeIcon-1024.png -resize 144x144 Sudoku9FreeIcon-72@2x.png 
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 72x72 Sudoku9FreeIcon-72.png 
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 144x144 Sudoku9FreeIcon-72@2x.png 
 
-convert.exe Sudoku9FreeIcon-1024.png -resize 512x512 Sudoku9FreeIcon-512.png 
-convert.exe Sudoku9FreeIcon-1024.png -resize 76x76 Sudoku9FreeIcon-76.png 
-convert.exe Sudoku9FreeIcon-1024.png -resize 152x152 Sudoku9FreeIcon-76@2x.png 
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 512x512 Sudoku9FreeIcon-512.png 
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 76x76 Sudoku9FreeIcon-76.png 
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 152x152 Sudoku9FreeIcon-76@2x.png 
+
+..\convert.exe Sudoku9FreeIcon-1024.png -resize 167x167 Sudoku9FreeIcon-83.5@2x.png 
