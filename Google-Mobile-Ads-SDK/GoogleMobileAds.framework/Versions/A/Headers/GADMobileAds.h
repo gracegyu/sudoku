380f88a55049e17b0507038b6dc25184c2ef2309
Raymond Jeon(Mac) <gracegyu@gmail.com>
2016-07-28 14:03:36 +0900

    Google Mobile Ads update

--- Google-Mobile-Ads-SDK/GoogleMobileAds.framework/Versions/A/Headers/GADMobileAds.h
+++ Google-Mobile-Ads-SDK/GoogleMobileAds.framework/Versions/A/Headers/GADMobileAds.h
@@ -12,6 +12,9 @@
 /// Returns the shared GADMobileAds instance.
 + (GADMobileAds *)sharedInstance;
 
+/// Configures the SDK using the settings associated with the given application ID.
++ (void)configureWithApplicationID:(NSString *)applicationID;
+
 /// Disables automated in app purchase (IAP) reporting. Must be called before any IAP transaction is
 /// initiated. IAP reporting is used to track IAP ad conversions. Do not disable reporting if you
 /// use IAP ads.
@@ -25,7 +28,12 @@
 /// The application's audio volume. Affects audio volumes of all ads relative to other audio output.
 /// Valid ad volume values range from 0.0 (silent) to 1.0 (current device volume). Use this method
 /// only if your application has its own volume controls (e.g., custom music or sound effect
-/// volumes).
+/// volumes). Defaults to 1.0.
 @property(nonatomic, assign) float applicationVolume;
+
+/// Indicates if the application's audio is muted. Affects initial mute state for all ads. Use this
+/// method only if your application has its own volume controls (e.g., custom music or sound effect
+/// muting). Defaults to NO.
+@property(nonatomic, assign) BOOL applicationMuted;
 
 @end
