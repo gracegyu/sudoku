0755a166ffe325d41924a91e7087bffa2603daea
Raymond Jeon(Mac) <gracegyu@gmail.com>
2017-07-27 12:14:43 +0900

    12.6인치 ipad 스크린샷을 요구함

--- iTunes/5.5to4.7and4.0for6.bat
+++ iTunes/5.5to4.7and4.0for6.bat
@@ -12,6 +12,20 @@ convert.exe sudoku6/en/5.5inch-4.jpg -resize 640x1136! sudoku6/en/4.0inch-4.jpg
 convert.exe sudoku6/en/5.5inch-5.jpg -resize 640x1136! sudoku6/en/4.0inch-5.jpg
 convert.exe sudoku6/en/5.5inch-6.jpg -resize 640x1136! sudoku6/en/4.0inch-6.jpg
 
+convert.exe sudoku6/en/iPad-1.jpg -resize 1536x2048! sudoku6/en/9.7iPad-1.jpg
+convert.exe sudoku6/en/iPad-2.jpg -resize 1536x2048! sudoku6/en/9.7iPad-2.jpg
+convert.exe sudoku6/en/iPad-3.jpg -resize 1536x2048! sudoku6/en/9.7iPad-3.jpg
+convert.exe sudoku6/en/iPad-4.jpg -resize 1536x2048! sudoku6/en/9.7iPad-4.jpg
+convert.exe sudoku6/en/iPad-5.jpg -resize 1536x2048! sudoku6/en/9.7iPad-5.jpg
+convert.exe sudoku6/en/iPad-6.jpg -resize 1536x2048! sudoku6/en/9.7iPad-6.jpg
+
+convert.exe sudoku6/en/iPad-1.jpg -resize 2048x2732! sudoku6/en/12.9iPad-1.jpg
+convert.exe sudoku6/en/iPad-2.jpg -resize 2048x2732! sudoku6/en/12.9iPad-2.jpg
+convert.exe sudoku6/en/iPad-3.jpg -resize 2048x2732! sudoku6/en/12.9iPad-3.jpg
+convert.exe sudoku6/en/iPad-4.jpg -resize 2048x2732! sudoku6/en/12.9iPad-4.jpg
+convert.exe sudoku6/en/iPad-5.jpg -resize 2048x2732! sudoku6/en/12.9iPad-5.jpg
+convert.exe sudoku6/en/iPad-6.jpg -resize 2048x2732! sudoku6/en/12.9iPad-6.jpg
+
 convert.exe sudoku6/ja/5.5inch-1.jpg -resize 750x1334! sudoku6/ja/4.7inch-1.jpg
 convert.exe sudoku6/ja/5.5inch-2.jpg -resize 750x1334! sudoku6/ja/4.7inch-2.jpg
 convert.exe sudoku6/ja/5.5inch-3.jpg -resize 750x1334! sudoku6/ja/4.7inch-3.jpg
@@ -25,6 +39,20 @@ convert.exe sudoku6/ja/5.5inch-3.jpg -resize 640x1136! sudoku6/ja/4.0inch-3.jpg
 convert.exe sudoku6/ja/5.5inch-4.jpg -resize 640x1136! sudoku6/ja/4.0inch-4.jpg
 convert.exe sudoku6/ja/5.5inch-5.jpg -resize 640x1136! sudoku6/ja/4.0inch-5.jpg
 convert.exe sudoku6/ja/5.5inch-6.jpg -resize 640x1136! sudoku6/ja/4.0inch-6.jpg
+
+convert.exe sudoku6/ja/iPad-1.jpg -resize 1536x2048! sudoku6/ja/9.7iPad-1.jpg
+convert.exe sudoku6/ja/iPad-2.jpg -resize 1536x2048! sudoku6/ja/9.7iPad-2.jpg
+convert.exe sudoku6/ja/iPad-3.jpg -resize 1536x2048! sudoku6/ja/9.7iPad-3.jpg
+convert.exe sudoku6/ja/iPad-4.jpg -resize 1536x2048! sudoku6/ja/9.7iPad-4.jpg
+convert.exe sudoku6/ja/iPad-5.jpg -resize 1536x2048! sudoku6/ja/9.7iPad-5.jpg
+convert.exe sudoku6/ja/iPad-6.jpg -resize 1536x2048! sudoku6/ja/9.7iPad-6.jpg
+
+convert.exe sudoku6/ja/iPad-1.jpg -resize 2048x2732! sudoku6/ja/12.9iPad-1.jpg
+convert.exe sudoku6/ja/iPad-2.jpg -resize 2048x2732! sudoku6/ja/12.9iPad-2.jpg
+convert.exe sudoku6/ja/iPad-3.jpg -resize 2048x2732! sudoku6/ja/12.9iPad-3.jpg
+convert.exe sudoku6/ja/iPad-4.jpg -resize 2048x2732! sudoku6/ja/12.9iPad-4.jpg
+convert.exe sudoku6/ja/iPad-5.jpg -resize 2048x2732! sudoku6/ja/12.9iPad-5.jpg
+convert.exe sudoku6/ja/iPad-6.jpg -resize 2048x2732! sudoku6/ja/12.9iPad-6.jpg
 
 convert.exe sudoku6/ko/5.5inch-1.jpg -resize 750x1334! sudoku6/ko/4.7inch-1.jpg
 convert.exe sudoku6/ko/5.5inch-2.jpg -resize 750x1334! sudoku6/ko/4.7inch-2.jpg
@@ -40,6 +68,20 @@ convert.exe sudoku6/ko/5.5inch-4.jpg -resize 640x1136! sudoku6/ko/4.0inch-4.jpg
 convert.exe sudoku6/ko/5.5inch-5.jpg -resize 640x1136! sudoku6/ko/4.0inch-5.jpg
 convert.exe sudoku6/ko/5.5inch-6.jpg -resize 640x1136! sudoku6/ko/4.0inch-6.jpg
 
+convert.exe sudoku6/ko/iPad-1.jpg -resize 1536x2048! sudoku6/ko/9.7iPad-1.jpg
+convert.exe sudoku6/ko/iPad-2.jpg -resize 1536x2048! sudoku6/ko/9.7iPad-2.jpg
+convert.exe sudoku6/ko/iPad-3.jpg -resize 1536x2048! sudoku6/ko/9.7iPad-3.jpg
+convert.exe sudoku6/ko/iPad-4.jpg -resize 1536x2048! sudoku6/ko/9.7iPad-4.jpg
+convert.exe sudoku6/ko/iPad-5.jpg -resize 1536x2048! sudoku6/ko/9.7iPad-5.jpg
+convert.exe sudoku6/ko/iPad-6.jpg -resize 1536x2048! sudoku6/ko/9.7iPad-6.jpg
+
+convert.exe sudoku6/ko/iPad-1.jpg -resize 2048x2732! sudoku6/ko/12.9iPad-1.jpg
+convert.exe sudoku6/ko/iPad-2.jpg -resize 2048x2732! sudoku6/ko/12.9iPad-2.jpg
+convert.exe sudoku6/ko/iPad-3.jpg -resize 2048x2732! sudoku6/ko/12.9iPad-3.jpg
+convert.exe sudoku6/ko/iPad-4.jpg -resize 2048x2732! sudoku6/ko/12.9iPad-4.jpg
+convert.exe sudoku6/ko/iPad-5.jpg -resize 2048x2732! sudoku6/ko/12.9iPad-5.jpg
+convert.exe sudoku6/ko/iPad-6.jpg -resize 2048x2732! sudoku6/ko/12.9iPad-6.jpg
+
 convert.exe sudoku6/zh_CN/5.5inch-1.jpg -resize 750x1334! sudoku6/zh_CN/4.7inch-1.jpg
 convert.exe sudoku6/zh_CN/5.5inch-2.jpg -resize 750x1334! sudoku6/zh_CN/4.7inch-2.jpg
 convert.exe sudoku6/zh_CN/5.5inch-3.jpg -resize 750x1334! sudoku6/zh_CN/4.7inch-3.jpg
@@ -54,6 +96,20 @@ convert.exe sudoku6/zh_CN/5.5inch-4.jpg -resize 640x1136! sudoku6/zh_CN/4.0inch-
 convert.exe sudoku6/zh_CN/5.5inch-5.jpg -resize 640x1136! sudoku6/zh_CN/4.0inch-5.jpg
 convert.exe sudoku6/zh_CN/5.5inch-6.jpg -resize 640x1136! sudoku6/zh_CN/4.0inch-6.jpg
 
+convert.exe sudoku6/zh_CN/iPad-1.jpg -resize 1536x2048! sudoku6/zh_CN/9.7iPad-1.jpg
+convert.exe sudoku6/zh_CN/iPad-2.jpg -resize 1536x2048! sudoku6/zh_CN/9.7iPad-2.jpg
+convert.exe sudoku6/zh_CN/iPad-3.jpg -resize 1536x2048! sudoku6/zh_CN/9.7iPad-3.jpg
+convert.exe sudoku6/zh_CN/iPad-4.jpg -resize 1536x2048! sudoku6/zh_CN/9.7iPad-4.jpg
+convert.exe sudoku6/zh_CN/iPad-5.jpg -resize 1536x2048! sudoku6/zh_CN/9.7iPad-5.jpg
+convert.exe sudoku6/zh_CN/iPad-6.jpg -resize 1536x2048! sudoku6/zh_CN/9.7iPad-6.jpg
+
+convert.exe sudoku6/zh_CN/iPad-1.jpg -resize 2048x2732! sudoku6/zh_CN/12.9iPad-1.jpg
+convert.exe sudoku6/zh_CN/iPad-2.jpg -resize 2048x2732! sudoku6/zh_CN/12.9iPad-2.jpg
+convert.exe sudoku6/zh_CN/iPad-3.jpg -resize 2048x2732! sudoku6/zh_CN/12.9iPad-3.jpg
+convert.exe sudoku6/zh_CN/iPad-4.jpg -resize 2048x2732! sudoku6/zh_CN/12.9iPad-4.jpg
+convert.exe sudoku6/zh_CN/iPad-5.jpg -resize 2048x2732! sudoku6/zh_CN/12.9iPad-5.jpg
+convert.exe sudoku6/zh_CN/iPad-6.jpg -resize 2048x2732! sudoku6/zh_CN/12.9iPad-6.jpg
+
 convert.exe sudoku6/zh_TW/5.5inch-1.jpg -resize 750x1334! sudoku6/zh_TW/4.7inch-1.jpg
 convert.exe sudoku6/zh_TW/5.5inch-2.jpg -resize 750x1334! sudoku6/zh_TW/4.7inch-2.jpg
 convert.exe sudoku6/zh_TW/5.5inch-3.jpg -resize 750x1334! sudoku6/zh_TW/4.7inch-3.jpg
@@ -67,3 +123,18 @@ convert.exe sudoku6/zh_TW/5.5inch-3.jpg -resize 640x1136! sudoku6/zh_TW/4.0inch-
 convert.exe sudoku6/zh_TW/5.5inch-4.jpg -resize 640x1136! sudoku6/zh_TW/4.0inch-4.jpg
 convert.exe sudoku6/zh_TW/5.5inch-5.jpg -resize 640x1136! sudoku6/zh_TW/4.0inch-5.jpg
 convert.exe sudoku6/zh_TW/5.5inch-6.jpg -resize 640x1136! sudoku6/zh_TW/4.0inch-6.jpg
+
+convert.exe sudoku6/zh_TW/iPad-1.jpg -resize 1536x2048! sudoku6/zh_TW/9.7iPad-1.jpg
+convert.exe sudoku6/zh_TW/iPad-2.jpg -resize 1536x2048! sudoku6/zh_TW/9.7iPad-2.jpg
+convert.exe sudoku6/zh_TW/iPad-3.jpg -resize 1536x2048! sudoku6/zh_TW/9.7iPad-3.jpg
+convert.exe sudoku6/zh_TW/iPad-4.jpg -resize 1536x2048! sudoku6/zh_TW/9.7iPad-4.jpg
+convert.exe sudoku6/zh_TW/iPad-5.jpg -resize 1536x2048! sudoku6/zh_TW/9.7iPad-5.jpg
+convert.exe sudoku6/zh_TW/iPad-6.jpg -resize 1536x2048! sudoku6/zh_TW/9.7iPad-6.jpg
+
+convert.exe sudoku6/zh_TW/iPad-1.jpg -resize 2048x2732! sudoku6/zh_TW/12.9iPad-1.jpg
+convert.exe sudoku6/zh_TW/iPad-2.jpg -resize 2048x2732! sudoku6/zh_TW/12.9iPad-2.jpg
+convert.exe sudoku6/zh_TW/iPad-3.jpg -resize 2048x2732! sudoku6/zh_TW/12.9iPad-3.jpg
+convert.exe sudoku6/zh_TW/iPad-4.jpg -resize 2048x2732! sudoku6/zh_TW/12.9iPad-4.jpg
+convert.exe sudoku6/zh_TW/iPad-5.jpg -resize 2048x2732! sudoku6/zh_TW/12.9iPad-5.jpg
+convert.exe sudoku6/zh_TW/iPad-6.jpg -resize 2048x2732! sudoku6/zh_TW/12.9iPad-6.jpg
+
