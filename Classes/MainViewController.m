5780de5fe87238efb0c3c1178c169dbc02fb4c2a
Raymond Jeon(Mac) <gracegyu@gmail.com>
2018-04-25 16:16:36 +0900

    SDSDKNINEP-3436 Crash 부분 제거

--- Classes/MainViewController.m
+++ Classes/MainViewController.m
@@ -3087,13 +3087,16 @@
 	
 	if (theResponseData)
 	{
+        /*
 		char str[6000+10];   // 3000 -> 6000 fo safe
 		
 		memcpy(&str, theResponseData.bytes, MIN(theResponseData.length, 3000));
         //str[theResponseData.length] = '\0';     // SDSDKNINEP-3432 Crash report
         str[MIN(theResponseData.length, 3000)] = '\0';
 		DLog(@"char* = %s", str);
-		
+		*/
+        
+        
 		NSString *strData = [[NSString alloc] initWithData:theResponseData encoding:NSUTF8StringEncoding];
 		
 		//		NSLog(@"strData = %@", strData);
