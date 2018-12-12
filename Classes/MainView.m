17d39470d049f9e9a72eca3115362f71e0160162
Raymond Jeon(Mac) <gracegyu@gmail.com>
2018-12-12 17:21:19 +0900

    crash 기록

--- Classes/MainView.m
+++ Classes/MainView.m
@@ -2081,7 +2081,7 @@ static NSUInteger SmallerColorTemplate[9] = {
     selectedXPos = xPos;
     selectedYPos = yPos;
     
-    // Crash here SDSDKNINEF-14364
+    // Crash here SDSDKNINEF-14364, SDSDKNINEP-3519
     if (sudokuGame.sudokuType == SUDOKUTYPE_GT) // 주변셀 대소비교 로직 시작
     {
         [sudokuGame setGtCellColors:selectedXPos y:selectedYPos];
