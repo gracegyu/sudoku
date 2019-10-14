7bb30baaeb903fe3e9851c158530c65f548836f1
Raymond Jeon(Mac) <gracegyu@gmail.com>
2019-10-14 10:00:20 +0900

    Detect iPhone version

--- Classes/AppDelegate.m
+++ Classes/AppDelegate.m
@@ -131,13 +131,12 @@ BOOL IsiPhoneX(void)
         NSString *model = [NSString stringWithCString:systemInfo.machine
                                              encoding:NSUTF8StringEncoding];
 #endif
-        DLog(@"model = %@", model);
+        int version = [[model substringWithRange:NSMakeRange(6,2)] intValue];
+        DLog(@"model = %@, %@, %d", model, [model substringWithRange:NSMakeRange(6,2)], version);
         isiPhoneX = [model isEqualToString:@"iPhone10,3"] ||
                     [model isEqualToString:@"iPhone10,6"] ||
-                    [model rangeOfString:@"iPhone11,"].location != NSNotFound ||
-                    [model rangeOfString:@"iPhone12,"].location != NSNotFound ||
-                    [model rangeOfString:@"iPhone13,"].location != NSNotFound ||
-                    [model rangeOfString:@"iPhone14,"].location != NSNotFound;
+                    [model rangeOfString:@"iPhone11,"].location != NSNotFound;
+        if (version > 11) isiPhoneX = YES;
     });
     
     return isiPhoneX;
